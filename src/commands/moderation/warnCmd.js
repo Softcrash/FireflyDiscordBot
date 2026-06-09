3ed41ac0e306c5e7beddcd26f33d794797cfd95a
CreodevHD <creomatic@proton.me>
2026-06-09 17:43:09 +0200

    cmd auf test stellen

--- src/commands/moderation/warnCmd.js
+++ src/commands/moderation/warnCmd.js
@@ -29,7 +29,7 @@ module.exports = {
   testMode: true,
   devOnly: false,
 
-  run: async (client, interaction) => {
+  run: async (client, interaction) => {gi
     await interaction.deferReply(EPHEMERAL);
 
     if (!(await canUseCommand(interaction.member, 'warn'))) {
